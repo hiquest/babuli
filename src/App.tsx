53216ee4f7375ae949eb552fe0720e003dd9ccbb
Ianis Triandafilov <janis.sci@gmail.com>
2025-03-24 18:41:40 +0100

    new weights

--- src/App.tsx
+++ src/App.tsx
@@ -21,22 +21,22 @@ const data = {
   anny: {
     start: 75_000,
     target: 70_000,
-    current: 73_800,
+    current: 73_300,
   },
   vt: {
     start: 78_000,
     target: 72_000,
-    current: 76_700,
+    current: 78_000,
   },
   tanya: {
     start: 77_500,
     target: 73_000,
-    current: 77_100,
+    current: 76_800,
   },
   yanis: {
     start: 67_500,
     target: 63_000,
-    current: 66_900,
+    current: 67_600,
   },
 };
 
